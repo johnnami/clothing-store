3de6f8930eab1c34d911dc7a96554bdc0826afb9
João Batista <73201919+johnnami@users.noreply.github.com>
2021-12-05 20:21:54 -0300

    Correções de erros

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,11 +1,29 @@
+//Função para pegar e obter variaveis no search ? categories = 1
+function getQueryParams(qs) {
+  qs = qs.split("+").join(" ");
+
+  var params = {},
+    tokens,
+    re = /[?&]?([^=]+)=([^&]*)/g;
+
+  while ((tokens = re.exec(qs))) {
+    params[decodeURIComponent(tokens[1])] = decodeURIComponent(tokens[2]);
+  }
+  return params;
+}
+
 const getProduct = async (id) => {
-  const produto = await get(`categories/${id}`);
+  //Pega o parametro que depois da interrogação chamando categories, caso esteja null ou undefined passa a ser 1 por causa da Null coalescing operator = ??
+  const idNumber = getQueryParams(window.location.search).categories ?? 1;
+  //Transforma URL get em categories e juntamente com o ID de cada mock
+  const produto = await get(`categories/${idNumber}`);
 
   return produto;
 };
 
 const getCategories = async () => {
-  const categories = await get("categories/list");
+  //Transforma o menu em get, redirecionando para determinada url
+  const categories = await get("list");
 
   return categories;
 };
@@ -22,7 +40,7 @@ btnMobile.addEventListener("click", toggleMenu);
 btnMobileClose.addEventListener("click", toggleMenu);
 
 const getList = async () => {
-  const categories = await get("list");
+  const categories = await get("categories/list");
   const menu = document.getElementById("nav-category");
 
   for (let i = 0; i < categories.items.length; i++) {
@@ -41,25 +59,27 @@ const getList = async () => {
 getList();
 
 const getProducts = async () => {
-  const urlString = window.location.href;
-  const url = new URL(urlString);
-  const paramId = url.searchParams.get("categories");
-  const convertId = String(paramId);
   const product_title = document.getElementById("product_title");
   const link_title = document.getElementById("link_title");
-  
-  if(convertId == "1"){
+  const convertId = getQueryParams(window.location.search).categories ?? 1;
+
+  //Retorno para não executar o código quando não existir o product_title
+  if (!product_title) {
+    return;
+  }
+
+  if (convertId == "1") {
     product_title.innerHTML = "Camisetas";
     link_title.innerHTML = "Camisetas";
-  }else if(convertId == "2"){
+  } else if (convertId == "2") {
     product_title.innerHTML = "Calças";
     link_title.innerHTML = "Calças";
-  }else if(convertId == "3"){
+  } else if (convertId == "3") {
     product_title.innerHTML = "Calçados";
     link_title.innerHTML = "Calçados";
   }
 
-  const allProducts = await get(convertId);
+  const allProducts = await getProduct(convertId);
 
   for (let i = 0; i < allProducts.items.length; i++) {
     const layout = `
--- public/scripts/service/api.js
+++ public/scripts/service/api.js
@@ -1,17 +1,21 @@
-const baseURL = "http://localhost:8888/api/V1/categories";
+// pegando o url da API
+const baseURL = "http://localhost:8888/api/V1";
 
+// Função para adapta fetch
 async function api(path, method) {
   const response = await fetch(`${baseURL}/${path}`, { method });
 
   return response.json();
 }
 
+//função para pegar o get
 async function get(path) {
   const response = await api(path, "GET");
 
   return response;
 }
 
+//função para pegar o post
 async function post(path) {
   const response = await api(path, "POST");
 
